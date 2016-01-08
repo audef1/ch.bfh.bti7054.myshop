51f1bc41b3375a82c845d66c141d2dd1709784dc
Nalet Meinen <nalet@bluewin.ch>
2016-01-08 16:04:10 +0100

    Product Stock management

--- admin/src/AppBundle/Controller/ProductStockController.php
+++ admin/src/AppBundle/Controller/ProductStockController.php
@@ -2,49 +2,85 @@
 
 namespace AppBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AppBundle\Entity\ProductStock;
+use AppBundle\Form\ProductStockType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
-class ProductStockController extends Controller
-{
+class ProductStockController extends Controller {
+
     /**
      * @Route("/ProdutStock")
      */
-    public function ProdutStockAction()
-    {
+    public function ProdutStockAction() {
+        $em = $this->getDoctrine()->getManager();
+        $products = $em
+                ->getRepository('AppBundle:Product')
+                ->createQueryBuilder('p')
+                ->select('p.productNumber')
+                ->distinct()
+                ->getQuery()
+                ->getResult();
+
+        
+        $repository = $this->getDoctrine()->getRepository('AppBundle:ProductStock');
+        foreach ($products as $productNumber) {
+            $stock = $repository->findByproductNumber($productNumber['productNumber']);
+            
+            if(count($stock) === 0) {
+                $newStock = new ProductStock();
+                $newStock->setProductNumber($productNumber['productNumber']);
+                $newStock->setStock(0);
+                $em->persist($newStock);
+                $em->flush();
+            }        
+        }
+
+        $stocks = $repository->findAll();
+
         return $this->render('AppBundle:ProductStock:produt_stock.html.twig', array(
-            // ...
+                    'stocks' => $stocks
         ));
     }
 
     /**
-     * @Route("/ProductStockNew")
+     * @Route("/ProductStockEdit/{id}")
      */
-    public function ProductStockNewAction()
-    {
-        return $this->render('AppBundle:ProductStock:product_stock_new.html.twig', array(
-            // ...
-        ));
-    }
+    public function ProductStockEditAction(Request $request, $id) {
+        $repository = $this->getDoctrine()->getRepository('AppBundle:ProductStock');
 
-    /**
-     * @Route("/ProductStockEdit")
-     */
-    public function ProductStockEditAction()
-    {
+        $stock = $repository->find($id);
+        $form = $this->createForm(ProductStockType::class, $stock);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid() && $form->isSubmitted()) {
+            $stock = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($stock);
+            $em->flush();
+
+            return $this->redirectToRoute('app_productstock_produtstock');
+        }
+
         return $this->render('AppBundle:ProductStock:product_stock_edit.html.twig', array(
-            // ...
+                    'form' => $form->createView()
         ));
     }
 
     /**
-     * @Route("/ProductStockDelete")
+     * @Route("/ProductStockDelete/{id}")
      */
-    public function ProductStockDeleteAction()
-    {
-        return $this->render('AppBundle:ProductStock:product_stock_delete.html.twig', array(
-            // ...
-        ));
+    public function ProductStockDeleteAction(Request $request, $id) {
+        $repository = $this->getDoctrine()->getRepository('AppBundle:ProductStock');
+
+        $stock = $repository->find($id);
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($stock);
+        $em->flush();
+
+        return $this->redirectToRoute('app_productstock_produtstock');
     }
 
 }
--- admin/src/AppBundle/Form/ProductStockType.php
+++ admin/src/AppBundle/Form/ProductStockType.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -17,6 +18,7 @@ class ProductStockType extends AbstractType
         $builder
             ->add('productNumber')
             ->add('stock')
+            ->add('save', SubmitType::class)
         ;
     }
     
