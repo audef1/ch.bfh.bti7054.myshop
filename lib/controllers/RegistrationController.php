0954a77f760824aa9f7bc11c6b9edf487adba2b5
Florian Auderset <f_auderset@hotmail.com>
2016-01-16 14:31:07 +0100

    progressing registration

--- lib/controllers/RegistrationController.php
+++ lib/controllers/RegistrationController.php
@@ -30,4 +30,48 @@ class RegistrationController
         $footer->render();
     }
 
+    public function registerCustomer(){
+
+        $salt = md5($_POST['firstname'] . $_POST['lastname']);
+        $password = hash('ripemd128', $_POST['password'] . $salt);
+
+        $db = DatabaseController::getInstance();
+        $mysqli = $db->getConnection();
+        $sql_query = "INSERT INTO `customer` (`customer_title`,
+                                              `customer_firstname`,
+                                              `customer_lastname`,
+                                              `customer_login`,
+                                              `customer_password`,
+                                              `customer_email`,
+                                              `customer_phone`,
+                                              `customer_address`,
+                                              `customer_zip`,
+                                              `customer_location`,
+                                              `customer_title2`,
+                                              `customer_firstname2`,
+                                              `customer_lastname2`,
+                                              `customer_address2`,
+                                              `customer_zip2`,
+                                              `customer_location2`,
+                                              `customer_billingaddress` )
+                                              VALUES ('$id', '$products');";
+
+
+
+
+
+
+        $mysqli->query($sql_query);
+        $mysqli->commit();
+
+        //get customernumber from name
+        //create hashed password and salt, store it in db
+
+        //get input from POST
+
+        //do query!
+
+
+    }
+
 }
